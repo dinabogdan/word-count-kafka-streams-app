1758c7fb69faaee4894695e9183271b2be408d78
bogdan.dina <bogdan.dina@orange.com>
2018-12-19 23:27:04 +0200

    setup kafka configurations

--- src/main/java/com/freesoft/kafka/streams/WordCountApp.java
+++ src/main/java/com/freesoft/kafka/streams/WordCountApp.java
@@ -1,8 +1,23 @@
 package com.freesoft.kafka.streams;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.util.Properties;
+
 public class WordCountApp {
 
     public static void main(String[] args) {
-        Properties properties 
+        setUpKafkaConfigurations();
+    }
+
+    private static void setUpKafkaConfigurations() {
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streams-word-count");
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "172.16.207.43:9092");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     }
 }
